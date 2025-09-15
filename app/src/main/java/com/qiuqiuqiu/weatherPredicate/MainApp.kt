d6a96cbc6f281b5fbb01f853772d9a41ad6ac810
qiuqiuqiu131 <2837316673@qq.com>
2025-09-15 15:39:22 +0800

    defaultCompose

--- app/src/main/java/com/qiuqiuqiu/weatherPredicate/MainApp.kt
+++ app/src/main/java/com/qiuqiuqiu/weatherPredicate/MainApp.kt
@@ -1,10 +1,14 @@
 package com.qiuqiuqiu.weatherPredicate
 
+import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -28,96 +32,14 @@ fun MainApp(modifier: Modifier = Modifier) {
         modifier = modifier
     )
     {
-        composable(
-            "Main",
-            enterTransition = {
-                slideIntoContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
-                    initialOffset = { fullSize ->
-                        fullSize / 3
-                    }
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
-                    targetOffset = { fullSize ->
-                        fullSize / 3
-                    }
-                )
-            }, popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow
-                    ),
-                    initialOffset = { fullSize ->
-                        fullSize / 3
-                    }
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    targetOffset = { fullSize ->
-                        fullSize / 3
-                    }
-                )
-            }) {
+        animatedNavComposable("Main", slideSizeExcept = 3) {
             MainScreen(navController)
         }
 
-        composable(
+        animatedNavComposable(
             "WeatherDetail?pageName={pageName}&pageInfo={pageInfo}&longitude={longitude}&latitude={latitude}",
-            arguments = listOf(),
-            enterTransition = {
-                slideIntoContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            }, popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            }) {
+            arguments = listOf()
+        ) {
             val pageName = it.arguments?.getString("pageName")
             val pageInfo = it.arguments?.getString("pageInfo")
             val longitude = it.arguments?.getString("longitude")?.toDoubleOrNull() ?: 116.4074
@@ -125,285 +47,92 @@ fun MainApp(modifier: Modifier = Modifier) {
             WeatherDetailScreen(navController, Pair(longitude, latitude), pageName, pageInfo)
         }
 
-        composable(
-            "WeatherSearch",
-            enterTransition = {
-                slideIntoContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            }, popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            }) {
+        animatedNavComposable("WeatherSearch") {
             WeatherSearchScreen(navController)
         }
 
-        composable(
+        animatedNavComposable(
             "CityWeather?longitude={longitude}&latitude={latitude}",
-            arguments = listOf(),
-            enterTransition = {
-                slideIntoContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            }, popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            }) {
+            arguments = listOf()
+        ) {
             val longitude = it.arguments?.getString("longitude")?.toDoubleOrNull() ?: 116.4074
             val latitude = it.arguments?.getString("latitude")?.toDoubleOrNull() ?: 39.9042
             WeatherCityScreen(navController, Pair(longitude, latitude))
         }
 
-        composable(
-            "CityManage",
-            enterTransition = {
-                slideIntoContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            }, popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            }) {
+        animatedNavComposable("CityManage") {
             CityManageScreen(navController)
         }
 
-        composable(
-            "CityEdit",
-            enterTransition = {
-                slideIntoContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            }, popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            }) {
+        animatedNavComposable("CityEdit") {
             CityEditScreen(navController)
         }
-        composable(
-            "time/global",
-            enterTransition = {
-                slideIntoContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            }, popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            }) {
+
+        animatedNavComposable("time/global") {
             GlobalTimeScreen(onBack = { navController.popBackStack() })
         }
-        composable(
-            "time/solar",
-            enterTransition = {
-                slideIntoContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            }, popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            }) {
+
+        animatedNavComposable("time/solar") {
             SolarTermScreen(onBack = { navController.popBackStack() })
         }
 
-        composable(
-            "time/city",
-            enterTransition = {
-                slideIntoContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    ),
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left
-                )
-            }, popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioNoBouncy,
-                        stiffness = Spring.StiffnessMediumLow,
-                    )
-                )
-            }) {
+        animatedNavComposable("time/city") {
             TourScreen(onBack = { navController.popBackStack() })
         }
     }
+}
+
+fun NavGraphBuilder.animatedNavComposable(
+    route: String,
+    arguments: List<NamedNavArgument> = emptyList(),
+    slideSizeExcept: Int = 1,
+    direction: AnimatedContentTransitionScope.SlideDirection = AnimatedContentTransitionScope.SlideDirection.Left,
+    popDirection: AnimatedContentTransitionScope.SlideDirection = AnimatedContentTransitionScope.SlideDirection.Right,
+    content: @Composable (AnimatedContentScope.(NavBackStackEntry) -> Unit)
+) {
+    composable(
+        route = route,
+        arguments = arguments,
+        enterTransition = {
+            slideIntoContainer(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessMediumLow
+                ),
+                towards = direction,
+                initialOffset = { fullSize -> fullSize / slideSizeExcept }
+            )
+        },
+        exitTransition = {
+            slideOutOfContainer(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessMediumLow
+                ),
+                towards = direction,
+                targetOffset = { fullSize -> fullSize / slideSizeExcept }
+            )
+        },
+        popEnterTransition = {
+            slideIntoContainer(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessMediumLow
+                ),
+                towards = popDirection,
+                initialOffset = { fullSize -> fullSize / slideSizeExcept }
+            )
+        },
+        popExitTransition = {
+            slideOutOfContainer(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessMediumLow
+                ),
+                towards = popDirection,
+                targetOffset = { fullSize -> fullSize / slideSizeExcept }
+            )
+        },
+        content = content
+    )
 }
